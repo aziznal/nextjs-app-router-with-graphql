2c057f4100f3ef327c581e8f56f125e2bdceed09
Abdulaziz Nal <abdulaziz.nal@anywai.ch>
2023-09-06 09:04:53 +0300

    setup prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Page() {
+import prisma from "@/server/prisma/prisma-client";
+
+export default async function Page() {
+  await prisma.todo.create({
+    data: {
+      title: "my todo",
+      description: "This is a todo!",
+    },
+  });
+
+  const todoCount = await prisma.todo.count();
+
   return (
     <div className="flex flex-col h-full items-center justify-center">
       <h1>Hello World</h1>
+
+      <span>You have {todoCount} todos!</span>
     </div>
   );
 }
